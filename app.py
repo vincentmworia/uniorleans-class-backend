148a8e796236dc51d943d9ab5cdf7031638d7d55
Vincent Mworia <mworiavince@gmail.com>
2025-11-11 15:35:06 +0100

    class 1 done

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask
 from flask import request
-from flask import render_template ###############
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("hello.html",name="Vince") 
+    return render_template("hello.html", name="Vince")
+
 
 @app.route("/custom")
 def custom_screen():
-    return render_template("hello2.html") 
+    return render_template("hello_vince.html")
+
 
 @app.route("/german")
 def german_home():
     return "Hallo, Flask! Wie get's ?"
 
+
 @app.route("/parrot")
 @app.route("/parrot/<sentence>")
 def parrot(sentence="Nothing"):
-    return render_template("yousay.html", what=sentence) 
+    return render_template("yousay.html", what=sentence)
+
 
 @app.route("/parameterized")
-def parameterized() :
-    what_in=request.args.get("what")
+def parameterized():
+    what_in = request.args.get("what")
     return render_template("youaskfor.html", what_out=what_in)
